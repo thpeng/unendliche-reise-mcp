6650e3861fa05b584cb95fadb45af9bbaadc18be
Thierry Peng <thierry.peng@gmail.com>
2026-02-06 14:47:38 +0100

    disabled security for the moment.

--- src/main/java/ch/thp/proto/unendlichereise/UnendlicheReiseApplication.java
+++ src/main/java/ch/thp/proto/unendlichereise/UnendlicheReiseApplication.java
@@ -6,6 +6,8 @@ import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class UnendlicheReiseApplication {
@@ -18,6 +20,19 @@ public class UnendlicheReiseApplication {
     public ToolCallbackProvider locationInfoTools(LocationInfoTool tool) {
         return MethodToolCallbackProvider.builder()
                 .toolObjects(tool)
+                .build();
+    }
+
+    @Bean
+    SecurityWebFilterChain mcpSecurityDisabledSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(reg -> reg
+                        // Everything else is public
+                        .anyExchange().permitAll()
+                )
                 .build();
     }
 }
